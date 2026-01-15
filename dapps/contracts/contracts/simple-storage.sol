7fe32ee578bd11234d0743c155415a17b038fe19
Dwiya <dwiyantoro54@gmail.com>
2026-01-15 14:52:48 +0700

    Ignore Hardhat artifacts and cache

--- dapps/contracts/contracts/simple-storage.sol
+++ dapps/contracts/contracts/simple-storage.sol
@@ -1,40 +1,20 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-
 contract SimpleStorage {
+    uint256 private storedValue;
     address public owner;
-    uint256 private value;
-    string private message;
-    // Events
-    event OwnerSet(address indexed oldOwner, address indexed newOwner);
-    event ValueUpdated(uint256 oldValue, uint256 newValue);
-    event MessageUpdated(string oldMessage, string newMessage);
+    event ValueUpdated(uint256 newValue);
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not owner");
-        _;
+    constructor(){
+    owner = msg.sender;
     }
-    constructor() {
-        owner = msg.sender;
-        emit OwnerSet(address(0), owner);
+
+    function setValue(uint256 _value) public {
+        storedValue = _value;
+        emit ValueUpdated(_value);
     }
-    // READ
+
     function getValue() public view returns (uint256) {
-        return value;
-    }
-
-    function getMessage() public view returns (string memory) {
-        return message;
-    }
-    // WRITE (ONLY OWNER)
-    function setValue(uint256 _value) public onlyOwner {
-        uint256 old = value;
-        value = _value;
-        emit ValueUpdated(old, _value);
-    }
-    function setMessage(string calldata _message) public onlyOwner {
-        string memory old = message;
-        message = _message;
-        emit MessageUpdated(old, _message);
+        return storedValue;
     }
 }
