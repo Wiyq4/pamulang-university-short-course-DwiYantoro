573dec4b64d89f7ccc06974e56890989b41a1b2c
Dwiya <dwiyantoro54@gmail.com>
2026-01-19 14:11:28 +0700

    fix: remove backend fetch from page.tsx

--- dapps/frontend/my-apps/app/page.tsx
+++ dapps/frontend/my-apps/app/page.tsx
@@ -1,25 +1,15 @@
-import { getBlockchainValue } from '@/services/blockchain.service';
 import ClientStorage from '@/components/ClientStorage';
 
-export default async function Page() {
-  const value = await getBlockchainValue();
-
+export default function Page() {
   return (
     <main className="min-h-screen p-6 space-y-6 text-white bg-black">
-      <header className="space-y-1">
+      <header>
         <h1 className="text-2xl font-bold">Simple Storage dApp</h1>
         <p className="text-sm text-gray-400">
           Dwi Yantoro · 231011403367
         </p>
       </header>
 
-      {/* READ (SERVER) */}
-      <section className="border border-white/10 rounded-lg p-4">
-        <p className="text-xs text-gray-400">LATEST VALUE (Server)</p>
-        <p className="text-4xl font-bold">{value.value}</p>
-      </section>
-
-      {/* WRITE (CLIENT) */}
       <ClientStorage />
     </main>
   );
